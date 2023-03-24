0f150be6c5d57167d40a75d6037b8f87d63d6cb5
Yeins <yeinsperez2004@gmail.com>
2023-03-23 23:35:53 -0500

    Se agrega el Proyecto

---
+++ scrips.js
@@ -0,0 +1,47 @@
+window.addEventListener('load',() =>{
+    const keyButons = document.getElementsByClassName('keypad-button');
+    const display = document.querySelector('.calculator-display');
+
+    const keyButtonsArray = Array.from(keyButons);
+
+    keyButtonsArray.forEach((button) =>{
+        button.addEventListener('click', ()=> {
+            //console.log(button)
+            calculadora(button, display)
+        });
+    });
+});
+
+function calculadora(button, display){
+    switch (button.innerHTML){
+        case 'C':
+            borrar(display);
+        break;
+        case '=':
+            resultado(display);
+        break;
+        default:
+            actualizar(display, button);
+        break;
+    }
+
+}
+
+function resultado(display){
+    display.innerHTML = eval(display.innerHTML);
+}
+
+function actualizar(display, button){
+    if(display.innerHTML ==0){
+        display.innerHTML = '';
+    }
+    display.innerHTML += button.innerHTML;
+}
+
+function borrar(display){
+    if(display.innerHTML != 0){
+        display.innerHTML = 0
+    }
+}
+
+
